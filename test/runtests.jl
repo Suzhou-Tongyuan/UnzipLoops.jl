516cb0017286944d21cc9e8cef3d110f9d42242a
Johnny Chen <johnnychen94@hotmail.com>
2022-11-04 13:03:43 +0800

    rename test functions

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,8 +4,8 @@ using OffsetArrays
 
 @testset "UnzipLoops" begin
     X, Y = rand(1:5, 1024), rand(1:5, 1024)
-    f(x, y) = x + y, x - y
-    function g(X, Y)
+    f1(x, y) = x + y, x - y
+    function g1(X, Y)
         @assert size(X) == size(Y)
         Base.require_one_based_indexing(X, Y)
 
@@ -14,39 +14,39 @@ using OffsetArrays
         Z1 = Array{T,N}(undef, size(X))
         Z2 = Array{T,N}(undef, size(X))
         @inbounds @simd for i in eachindex(X)
-            v = f(X[i], Y[i])
+            v = f1(X[i], Y[i])
             Z1[i] = v[1]
             Z2[i] = v[2]
         end
         return Z1, Z2
     end
-    Z1, Z2 = g(X, Y)
-    Z1′, Z2′ = broadcast_unzip(f, X, Y)
+    Z1, Z2 = g1(X, Y)
+    Z1′, Z2′ = broadcast_unzip(f1, X, Y)
     @test Z1 == Z1′
     @test Z2 == Z2′
 
     X, Y, Z = rand(1:5, 1024), rand(1:5, 1024), rand(1:5, 1024)
-    f(x, y, z) = x ^ y ^ z, x / y / z, x * y * z, x / (y*z)
-    out = broadcast_unzip(f, X, Y, Z)
-    out_aos = map(f, X, Y, Z)
+    f2(x, y, z) = x ^ y ^ z, x / y / z, x * y * z, x / (y*z)
+    out = broadcast_unzip(f2, X, Y, Z)
+    out_aos = map(f2, X, Y, Z)
     @test out[1] == getindex.(out_aos, 1)
     @test out[2] == getindex.(out_aos, 2)
     @test out[3] == getindex.(out_aos, 3)
     @test out[4] == getindex.(out_aos, 4)
 
-    f(x) = x, x^2, x^3
-    A, B, C = broadcast_unzip(f, [1, 2, 3])
-    @test collect(zip(A, B, C)) == broadcast(f, [1, 2, 3])
+    f3(x) = x, x^2, x^3
+    A, B, C = broadcast_unzip(f3, [1, 2, 3])
+    @test collect(zip(A, B, C)) == broadcast(f3, [1, 2, 3])
 
     # mixed axes are supported
-    f(x, y) = x + y, x - y
-    A, B = broadcast_unzip(f, [1, 2, 3], [4 5 6])
-    @test collect(zip(A, B)) == broadcast(f, [1, 2, 3], [4 5 6])
+    f4(x, y) = x + y, x - y
+    A, B = broadcast_unzip(f4, [1, 2, 3], [4 5 6])
+    @test collect(zip(A, B)) == broadcast(f4, [1, 2, 3], [4 5 6])
 
     # offsetted arrays
     xo = OffsetArray([1, 2, 3], -1)
-    A, B = broadcast_unzip(f, xo, xo)
+    A, B = broadcast_unzip(f4, xo, xo)
     @test axes(A) == (0:2,)
     @test axes(B) == (0:2,)
-    @test collect(zip(A, B)) == broadcast(f, xo, xo)
+    @test collect(zip(A, B)) == broadcast(f4, xo, xo)
 end
